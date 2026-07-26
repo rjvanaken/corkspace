2c81421bdb47c0dce055461abd30ac959d6ddeed
rjvanaken <rjvanaken@gmail.com>
2026-07-26 12:26:07 -0400

    Set up column headers

--- src/components/custom/Board/Column.tsx
+++ src/components/custom/Board/Column.tsx
@@ -1,7 +1,13 @@
+import { Circle } from "lucide-react";
+
 export default function Column({ label }: { label: string }) {
   return (
-    <div className="flex-1 h-full flex items-center justify-center text-muted-foreground text-xs">
-      <p>{label} placeholder</p>
+    <div className="flex-1 flex-col h-full w-full flex items-center justify-center text-muted-foreground text-xs">
+  <div className="px-6 py-4 w-full items-center justify gap-5 flex-row flex-1 flex">
+    <p className="text-xl font-bold">{label}</p>
+    <Circle />
+  </div>
+      <p className="w-full h-full text-center">{label} placeholder</p>
     </div>
   );
 }
