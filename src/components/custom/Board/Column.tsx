2cf694047ab1c649ac0a30c77587da5502b5bb38
rjvanaken <rjvanaken@gmail.com>
2026-07-26 13:06:55 -0400

    added card structure

--- src/components/custom/Board/Column.tsx
+++ src/components/custom/Board/Column.tsx
@@ -1,13 +1,18 @@
+import {Car, Circle} from 'lucide-react'
+import {Card} from '@/components/ui/card'
+
 export default function Column({ label }: { label: string }) {
   return (
-    <div className="flex-1 max-w-[800px] flex flex-col h-full bg-card rounded-lg overflow-hidden">
-      <div className="flex items-center gap-2 px-4 py-3 shrink-0">
-        <span className="text-sm font-bold text-foreground">{label}</span>
-        <div className="h-6 w-6 rounded-full border border-border"></div>
-      </div>
+    <div className="flex-1 max-w-[800px] gap-2 flex flex-col h-full bg-card rounded-lg overflow-hidden">
+      <div className="flex items-start h-screen flex-col gap-4 px-6 py-6 shrink-0">
+        <div className='flex items-center gap-3'>
+        <span className="text-xl font-bold text-foreground">{label}</span>
+        <Circle/>
+        </div>
+        <div className='flex items-center gap-3'>
+        <Card>Hi</Card>
 
-      <div className="flex-1 overflow-y-auto px-4">
-        <p className="text-xs text-muted-foreground">{label} placeholder</p>
+        </div>
       </div>
     </div>
   );
