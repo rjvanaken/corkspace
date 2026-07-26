497dac22c44cd1d73585cbc78c86d8f4aef8d0a2
rjvanaken <rjvanaken@gmail.com>
2026-07-26 11:56:18 -0400

    added borders to columns

--- src/components/custom/Board.tsx
+++ src/components/custom/Board.tsx
@@ -2,11 +2,17 @@ import Column from "@/components/custom/Board/Column";
 
 export default function Board() {
   return (
-    <div className="flex-1 flex">
+    <div className="flex-1 flex h-screen">
       <Column label="To Do"></Column>
-      <Column label="In Progress"></Column>
-      <Column label="In Review"></Column>
-      <Column label="Done"></Column>
+      <div className="border-l border-border flex-1 flex">
+        <Column label="In Progress"></Column>
+      </div>
+      <div className="border-l border-border flex-1 flex">
+        <Column label="In Review"></Column>
+      </div>
+      <div className="border-l border-border flex-1 flex">
+        <Column label="Done"></Column>
+      </div>
     </div>
   );
 }
